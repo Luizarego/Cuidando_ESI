06536400938f4dec4a1de49c9572595f9fd7bdf7
Larissa Alvim <104645538+LarissaTerto@users.noreply.github.com>
2024-11-01 11:15:18 -0300

    Update SelecionarAno_step.rb

--- testes/features/step_definitions/SelecionarAno_step.rb
+++ testes/features/step_definitions/SelecionarAno_step.rb
@@ -1,17 +1,21 @@
+require 'capybara/cucumber'
+require 'selenium-webdriver'
+require 'capybara/rspec'
+
 #language: pt
-Dado('que estou na página inicial do site "Cuidando do Meu Bairro"') do
+Dado('Eu estou no início do site "Cuidando do Meu Bairro"') do
   visit '/' 
 end
 
-Quando('clico no botão de selecionar ano') do
-  expect(page).to have_selector('#navbar > ul > li:nth-child(6) > a') 
-  find('#navbar > ul > li:nth-child(6) > a').click 
+Quando('clico no elemento de selecionar ano') do
+  expect(page).to have_selector('#app > div > div.c-map.expanded-map > div.map__card-content.container.mx-auto > div > div.bg-white.lg\:pb-10.lg\:p-10.pt-10.pb-3.mb-5.lg\:rounded-lg.lg\:shadow-lg > div:nth-child(2) > div.c-map__year-submit.grid.grid-cols-2.gap-4.h-8 > div:nth-child(1) > div') 
+  find('#app > div > div.c-map.expanded-map > div.map__card-content.container.mx-auto > div > div.bg-white.lg\:pb-10.lg\:p-10.pt-10.pb-3.mb-5.lg\:rounded-lg.lg\:shadow-lg > div:nth-child(2) > div.c-map__year-submit.grid.grid-cols-2.gap-4.h-8 > div:nth-child(1) > div').click 
 end
 
 E('seleciono "2003"') do
-  find('-').click 
+  find('#app > div > div.c-map.expanded-map > div.map__card-content.container.mx-auto > div > div.bg-white.lg\:pb-10.lg\:p-10.pt-10.pb-3.mb-5.lg\:rounded-lg.lg\:shadow-lg > div:nth-child(2) > div.c-map__year-submit.grid.grid-cols-2.gap-4.h-8 > div:nth-child(1) > div > div > ul > li:nth-child(1)').click 
 end
 
-Então('devo ser redirecionado para a página de 2003: "https://cuidando.vc/2003"') do
+Então('devo ser redirecionado para a página de 2003') do
   assert_current_path('https://cuidando.vc/2003')
 end
