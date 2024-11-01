af2c125a78741d9463d057ad4668f0c127dd6bd0
Larissa Alvim <104645538+LarissaTerto@users.noreply.github.com>
2024-11-01 11:15:36 -0300

    Update LoginInvalido_step.rb

--- testes/features/step_definitions/LoginInvalido_step.rb
+++ testes/features/step_definitions/LoginInvalido_step.rb
@@ -6,13 +6,14 @@ Given('Eu estou na página de login') do
   visit('/')
   expect(page).to have_selector('#navbar > ul > li:nth-child(5) > a') 
   find('#navbar > ul > li:nth-child(5) > a').click
-  expect(page).to have_selector('form#login_form') # Verifica se o formulário de login foi carregado
+  expect(page).to have_selector('#modal-dialog > div') # Verifica se o formulário de login foi carregado
 end
 
 When('Eu insiro credenciais inválidas e envio') do
   fill_in 'Nome de usuário', with: 'usuario_invalido@mail.com'
   fill_in 'Senha', with: 'senha12345'  
-  click_button 'Entrar'
+  expect(page).to have_selector('#modal-dialog > div > div:nth-child(2) > div > form > button') 
+  find('#modal-dialog > div > div:nth-child(2) > div > form > button').click 
 end
 
 Then('Eu devo ver uma mensagem de erro de login "O nome de usuário parece não registrado..."') do
