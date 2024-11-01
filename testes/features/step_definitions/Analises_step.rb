1d8e101dbc26cf0f8f65110235c99843950d51d6
Larissa Alvim <104645538+LarissaTerto@users.noreply.github.com>
2024-11-01 11:16:47 -0300

    Update Analises_step.rb

--- testes/features/step_definitions/Analises_step.rb
+++ testes/features/step_definitions/Analises_step.rb
@@ -1,13 +1,17 @@
+require 'capybara/cucumber'
+require 'selenium-webdriver'
+require 'capybara/rspec'
+
 #language: pt
-Dado('que estou na página inicial do site "Cuidando do Meu Bairro"') do
+Dado('que estou na homepage') do
   visit '/'
 end
 
 Quando('clico em "Análises"') do
-  expect(page).to have_selector('-') 
-  find('-').click 
+  expect(page).to have_selector('#navbar > ul > li:nth-child(3) > a') 
+  find('#navbar > ul > li:nth-child(3) > a').click 
 end
 
-Então('devo ser redirecionado para a página de analises') do
+Então('devo ser redirecionado para a página de análises') do
   assert_current_path('https://cuidando.vc/analises')
 end
