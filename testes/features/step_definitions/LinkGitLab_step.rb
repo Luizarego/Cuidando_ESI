34ca57385be192f7a4bd9bfbf547d0fc1b5bd707
Larissa Alvim <104645538+LarissaTerto@users.noreply.github.com>
2024-11-01 11:16:16 -0300

    Update LinkGitLab_step.rb

--- testes/features/step_definitions/LinkGitLab_step.rb
+++ testes/features/step_definitions/LinkGitLab_step.rb
@@ -2,16 +2,17 @@ require 'capybara/cucumber'
 require 'selenium-webdriver'
 require 'capybara/rspec'
 
-Given('que estou na página inicial do site "Cuidando do Meu Bairro"') do
+Given('que estou na página de início') do
   visit('/')
 end
 
-When('desço para o footer') do
+When('vou para o footer') do
   execute_script('window.scrollTo(0, document.body.scrollHeight)')
 end
 
-When('clico no botão {string}') do |link_text|
-  click_link(link_text)
+When('clico em "GitLab"') do
+  expect(page).to have_selector('#app > footer > div > div > div:nth-child(1) > a:nth-child(2)') 
+  find('#app > footer > div > div > div:nth-child(1) > a:nth-child(2)').click 
 end
 
 Then('devo ser redirecionado para a página do GitLab') do
